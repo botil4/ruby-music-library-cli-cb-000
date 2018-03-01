16b520a3634273003c1f63d9ab212b3a8112c2f8
Travis Clarkson <tk1667@gmail.com>
2018-03-01 02:15:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,12 +38,12 @@ class Song
     song
   end
 
-  def self.find_by_name(song)
-    @@all.find{|song| song}
+  def self.find_by_name(name)
+    @@all.find{|song| song.name}
   end
 
-  def self.find_or_create_by_name
-
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
   end
 
 end
