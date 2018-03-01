9edfd9302afa7527b5072e82867d4e9d0a1b32f6
Travis Clarkson <tk1667@gmail.com>
2018-03-01 02:56:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,4 +47,12 @@ class Song
     self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
   end
 
+  def self.new_from_filename(file)
+
+  end
+
+  def self.create_from_filename(file)
+
+  end
+
 end
