300072d20959d5384ca81283e406731eb43bb91a
Travis Clarkson <tk1667@gmail.com>
2018-03-01 01:04:04 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.save
+    @@all << self 
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+  end
+
+end
